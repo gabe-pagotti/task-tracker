6dec7ef0846c2e313e1de4cd0f0c46766f6850e4
Gabriel Pagotti <pagotti46@gmail.com>
2021-10-23 15:37:57 -0300

    fix: Corrigindo posição do apóstrofo no atalho

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="[task.reminder ? 'reminder' : '', 'task']">
-        <h3>{{ task.text }} <i @click="$emit('delete-task, task.id')" class="fas fa-times"></i></h3>
+        <h3>{{ task.text }} <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i></h3>
         <p>{{ task.day }}</p>
     </div>
 </template>
