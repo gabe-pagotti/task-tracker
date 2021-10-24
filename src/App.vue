ee3bc4e44ab40b520e6b8dd33b086c49f41b8d50
Gabriel Pagotti <pagotti46@gmail.com>
2021-10-24 10:11:33 -0300

    feat: Adicionando conexão com uma API para o CRUD da task

--- src/App.vue
+++ src/App.vue
@@ -27,48 +27,85 @@ export default {
         }
     },
     methods: {
-        deleteTask(id) {
+        async deleteTask(id) {
             if (! confirm('Are you sure about that?')) {
+                return;
+            }
+
+            const res = await fetch(`/api/tasks/${id}`, {method: 'DELETE'});
+
+            if (res.status !== 200) {
+                alert('Error deleting task');
                 return;
             }
 
             this.tasks = this.tasks.filter((task) => task.id !== id);
         },
-        toggleReminder(id) {
-            this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task);
+        async toggleReminder(id) {
+            const task = await this.fetchTask(id);
+            const toggledTask = {...task, reminder: !task.reminder};
+
+            const res = await fetch(`/api/tasks/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(toggledTask)
+            });
+
+            const data = await res.json();
+
+            this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task);
+
+            // How I did before seeing the video
+            // const task = await this.fetchTask(id);
+
+            // const res = await fetch(`/api/tasks/${id}`, {
+            //     method: 'PATCH',
+            //     headers: {
+            //         'Content-type': 'application/json',
+            //     },
+            //     body: JSON.stringify({reminder: !task.reminder})
+            // });
+
+            // this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task);
         },
         toggleAddTask(id) {
             this.showAddTask = ! this.showAddTask;
         },
-        addTask(task) {
+        async addTask(task) {
+            const res = await fetch('/api/tasks', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(task)
+            });
 
-            this.tasks = [...this.tasks, task];
+            const data = await res.json();
+
+            this.tasks = [...this.tasks, data];
 
             // The way I would do
-            // this.tasks.push(task);
+            // this.tasks.push(data);
+        },
+        async fetchTasks() {
+            const res = await fetch('api/tasks');
+
+            const data = await res.json();
+
+            return data;
+        },
+        async fetchTask(id) {
+            const res = await fetch(`api/tasks/${id}`);
+
+            const data = await res.json();
+
+            return data;
         }
     },
-    created() {
-        this.tasks = [
-            {
-                id: 1,
-                text: 'Doctors Appointment',
-                day: 'March 1st at 2:30pm',
-                reminder: true,
-            },
-            {
-                id: 2,
-                text: 'Meeting at School',
-                day: 'March 3rd at 1:30pm',
-                reminder: true,
-            },
-            {
-                id: 3,
-                text: 'Food Shopping',
-                day: 'March 3rd at 11:00am',
-                reminder: false,
-            }
-        ]
+    async created() {
+        this.tasks = await this.fetchTasks();
     }
 }
 </script>
